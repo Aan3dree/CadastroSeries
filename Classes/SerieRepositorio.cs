4d7ee74bc8c4cfa8cc58d627af3960661cd82974
André Luiz Silva de Souza <andre.silva.de.souza@avanade.com>
2021-04-13 16:46:05 -0300

    first commit

---
+++ Classes/SerieRepositorio.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using CadastroSeries.Interface;
+
+namespace CadastroSeries
+{
+    public class SerieRepositorio : IRepositorio<Serie>
+    {
+        private List<Serie> listaSerie = new List<Serie>();
+        public void Atualiza(int id, Serie entidade)
+        {
+            listaSerie[id] = entidade;
+        }
+
+        public void Exclui(int id)
+        {
+            listaSerie.RemoveAt(id);
+        }
+
+        public void Insere(Serie entidade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Serie> Lista()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int ProximoId()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Serie RetornaPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
